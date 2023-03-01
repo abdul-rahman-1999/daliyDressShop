882b95b373d6371cd0b96dc542ad452a1df4580b
abdul-rahman-1999 <rahmanabdul10@outlook.com>
2023-03-01 20:01:43 +0530

    payment

--- src/components/Stripecheckout.js
+++ src/components/Stripecheckout.js
@@ -32,7 +32,7 @@ function Stripecheckout() {
 
   return <>
   
-   <StripeCheckout amount={total_price * 100} shippingAddress token={tokenHandler} currency="INR" stripeKey="pk_test_51MgiTbSFzVFGx5mXeir29Et6NdzZv7uelev0WMvIj3xiJ3bedFpLoJsngwq2g9ZQyZvcpMYbjUGKXM8ZxFERrBu700dxEDcJQC">
+   <StripeCheckout amount={total_price * 100} shippingAddress token={tokenHandler} currency="INR" stripeKey={process.env.REACT_APP_STRIPE_KEY}>npm start
 
    <Button variant="contained" sx={{backgroundColor:"#8E3A59",color:"white",padding:"7px 15px",margin:"8px 0px 8px 0px",width:"100%"}}>Make Payment</Button>
    <ToastContainer />
